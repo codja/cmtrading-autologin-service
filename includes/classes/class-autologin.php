f60a4c20146d93797b934bb37d0f85c83f9ed4a0
RuslanKos1989 <arrusl@yandex.ru>
2024-10-01 13:58:12 +0300

    [ AT-197-autologin-upgrade ] if sso link not found we redirect to autologin link

--- includes/classes/class-autologin.php
+++ includes/classes/class-autologin.php
@@ -67,10 +67,13 @@ class Autologin {
 			wp_die( esc_html__( 'CRM DB error. Account not found.', 'cmtrading-autologin' ), '', [ 'response' => 403 ] );
 		}
 
-		$provider          = new Antelope();
-		$link_for_redirect = $is_platform_link
-			? $this->get_sso_link( $provider, $asset, $account_id )
-			: $provider->get_autologin_link( $account_id );
+		$provider = new Antelope();
+		// Let's try to get SSO link first, if necessary
+		$link_for_redirect = $is_platform_link ? $this->get_sso_link( $provider, $asset, $account_id ) : null;
+		// If the SSO link was not found, we get an autologin link
+		if ( ! $link_for_redirect ) {
+			$link_for_redirect = $provider->get_autologin_link( $account_id );
+		}
 
 		if ( ! $link_for_redirect ) {
 			wp_die( esc_html__( 'CRM API error. Contact the administrator.', 'cmtrading-autologin' ), '', [ 'response' => 403 ] );
